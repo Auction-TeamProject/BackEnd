8f41141d40befc897feae8355005c30ac43819e9
seomyeong-gwan <myeonggwanseo70@naver.com>
2024-12-17 20:11:47 +0900

    예외 처리 전용 컨트롤러

---
+++ src/main/java/com/auction/auction_site/controller/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.auction.auction_site.controller;
+
+import com.auction.auction_site.dto.ApiResponse;
+import com.auction.auction_site.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@ControllerAdvice
+public class ExceptionController { // 예외 처리용 컨트롤러
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse illegalExHandle(IllegalStateException e) {
+        return new ErrorResponse("BAD_REQUEST", e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public ErrorResponse handleAuthenticationException(AuthenticationException e) {
+        return new ErrorResponse("UNAUTHORIZED", e.getMessage());
+    }
+}
