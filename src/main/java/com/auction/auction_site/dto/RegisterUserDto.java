bee25a17a9c792b1abd1a8f12f992079d8257ea0
myeonggwan00 <myeonggwanseo70@naver.com>
2024-12-20 23:58:58 +0900

    Refactor: 코드 리팩토링
    빌더 패턴 추가, user 엔티티를 DTO로 변경하는 정적 팩토리 메서드 생성

--- src/main/java/com/auction/auction_site/dto/RegisterUserDto.java
+++ src/main/java/com/auction/auction_site/dto/RegisterUserDto.java
@@ -1,8 +1,13 @@
 package com.auction.auction_site.dto;
 
+import com.auction.auction_site.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
+@Builder
 @Getter @Setter
 public class RegisterUserDto {
     private String username;
@@ -17,5 +22,17 @@ public class RegisterUserDto {
 
     private String accountNumber;
 
-    private String role;
+    private Role role;
+
+    public static RegisterUserDto fromUser(User user) {
+        return RegisterUserDto.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .nickname(user.getNickname())
+                .phoneNumber(user.getPhoneNumber())
+                .address(user.getAddress())
+                .accountNumber(user.getAccountNumber())
+                .role(user.getRole())
+                .build();
+    }
 }
