e285f13a816666611e301681d0e097c3d048fabf
myeonggwan00 <myeonggwanseo70@naver.com>
2024-12-26 13:43:13 +0900

    - 수정

--- src/main/java/com/auction/auction_site/repository/UserRepository.java
+++ src/main/java/com/auction/auction_site/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.auction.auction_site.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     User findByUsername(String username);
