4f9424e0585428af5fb4a693456c68a75ed33754
myeonggwan00 <myeonggwanseo70@naver.com>
2024-12-28 21:27:17 +0900

    Feat: 스케줄링 기능 추가
    스케줄링 작업으로 만료시간이 지난 Refresh 토큰 삭제

--- src/main/java/com/auction/auction_site/repository/RefreshTokenRepository.java
+++ src/main/java/com/auction/auction_site/repository/RefreshTokenRepository.java
@@ -3,11 +3,19 @@ package com.auction.auction_site.repository;
 import com.auction.auction_site.entity.RefreshToken;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Boolean existsByRefreshToken(String refreshToken);
 
     @Transactional
     void deleteByRefreshToken(String refreshToken);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM RefreshToken r WHERE r.expiration < :now")
+    void deleteExpiredRefresh(Date now);
 }
